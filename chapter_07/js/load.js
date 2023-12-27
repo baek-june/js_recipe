dd2ef67b017e6eb6efb76518973a21fcf5654a38
성백준 <toypiano@naver.com>
2023-12-27 12:57:34 +0900

    first commit

---
+++ chapter_07/js/load.js
@@ -0,0 +1,8 @@
+// DOM 엑세스 타이밍에 처리 실행
+const boxNum = document.querySelectorAll('.box').length;
+console.log(`.box 요소의 개수는 ${boxNum}개입니다.`);
+
+// window.addEventListener('DOMContentLoaded', () => {
+//   const boxNum = document.querySelectorAll('.box').length;
+//   console.log(`.box 요소의 개수는 ${boxNum}개입니다.`);
+// });
